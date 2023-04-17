95f1389f814a58434fcc9016e4c925234462cb67
Ibarrx <angelibarra8@gmail.com>
2023-04-16 21:30:11 -0700

    ligger

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
+// Vertices coordinates
 GLfloat vertices[] =
-{
-	-0.5f,-0.5f,0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	-0.5f, 0.5f,0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-	 0.5f, 0.5f,0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-	 0.5f,-0.5f,0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
 };
+
+GLuint indices[] =
+{
+	0, 2, 1, // Upper triangle
+	0, 3, 2 // Lower triangle
+};
+
 
 int main()
 {
@@ -42,25 +50,26 @@ int main()
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
-	int widthImg, heightImg, numColch;
-	unsigned char* bytes = stbi_load("bg.jpg", &widthImg, &heightImg, &numColch, 0);
 
-	GLuint texture;
-	glGenTextures(1, &texture);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	//int widthImg, heightImg, numColch;
+	//unsigned char* bytes = stbi_load("bg.jpg", &widthImg, &heightImg, &numColch, 0);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//GLuint texture;
+	//glGenTextures(1, &texture);
+	//glActiveTexture(GL_TEXTURE0);
+	//glBindTexture(GL_TEXTURE_2D, texture);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
-	glGenerateMipmap(GL_TEXTURE_2D); 
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
-	stbi_image_free(bytes);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	//glGenerateMipmap(GL_TEXTURE_2D); 
+
+	//stbi_image_free(bytes);
+	//glBindTexture(GL_TEXTURE_2D, 0);
 
 	//While events are going on in window, show the window
 	while (!glfwWindowShouldClose(window))
@@ -68,7 +77,7 @@ int main()
 		glfwPollEvents();
 	}
 
-	glDeleteTextures(1, &texture);
+	/*glDeleteTextures(1, &texture);*/
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
