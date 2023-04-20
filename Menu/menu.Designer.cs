0c4ad1edb49bf80fad3edc24820040da8b92361b
Ibarrx <angelibarra8@gmail.com>
2023-04-19 21:15:22 -0700

    i hate harold

--- Menu/menu.Designer.cs
+++ Menu/menu.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu));
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkAudio = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartGame.BackColor = System.Drawing.Color.White;
+            this.btnStartGame.BackgroundImage = global::Menu.Properties.Resources.MenuBtn;
+            this.btnStartGame.Font = new System.Drawing.Font("NiseSegaSonic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStartGame.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStartGame.Image = global::Menu.Properties.Resources.MenuBtn;
+            this.btnStartGame.Location = new System.Drawing.Point(251, 251);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(296, 48);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "Start Game";
+            this.btnStartGame.UseVisualStyleBackColor = false;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSettings.BackColor = System.Drawing.Color.Transparent;
+            this.btnSettings.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSettings.Image = ((System.Drawing.Image)(resources.GetObject("btnSettings.Image")));
+            this.btnSettings.Location = new System.Drawing.Point(1, 395);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(68, 56);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.UseVisualStyleBackColor = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkAudio
+            // 
+            this.checkAudio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkAudio.AutoSize = true;
+            this.checkAudio.BackColor = System.Drawing.Color.Transparent;
+            this.checkAudio.Checked = true;
+            this.checkAudio.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkAudio.Font = new System.Drawing.Font("NiseSegaSonic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkAudio.ForeColor = System.Drawing.Color.White;
+            this.checkAudio.Location = new System.Drawing.Point(723, 435);
+            this.checkAudio.Name = "checkAudio";
+            this.checkAudio.Size = new System.Drawing.Size(77, 16);
+            this.checkAudio.TabIndex = 2;
+            this.checkAudio.Text = "Audio";
+            this.checkAudio.UseVisualStyleBackColor = false;
+            this.checkAudio.CheckedChanged += new System.EventHandler(this.checkAudio_CheckedChanged);
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Menu.Properties.Resources.back;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.checkAudio);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnStartGame);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "menu";
+            this.Text = "RockTheStick";
+            this.Load += new System.EventHandler(this.menu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnStartGame;
+        private Button btnSettings;
+        private OpenFileDialog openFileDialog1;
+        private CheckBox checkAudio;
     }
 }
--- Menu/menu.cs
+++ Menu/menu.cs
@@ -1,10 +1,56 @@
+using System.Diagnostics;
+using System.Media;
+
 namespace Menu
 {
     public partial class menu : Form
     {
+        private SoundPlayer soundPlayer; // Declare as a class-level variable
+
         public menu()
         {
             InitializeComponent();
+            soundPlayer = new SoundPlayer(@"C:\Users\Angel\source\repos\Rockthestick\sounds\MenuLoopable.wav"); // Initialize in the constructor
+        }
+
+        private void playMenuMusic()
+        {
+            soundPlayer.PlayLooping(); // Use PlayLooping() instead of Play()
+        }
+
+        private void menu_Load(object sender, EventArgs e)
+        {
+            
+            if(checkAudio.Checked)
+            {
+                playMenuMusic();
+            }
+
+        }
+
+        private void checkAudio_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkAudio.Checked)
+            {
+                playMenuMusic();
+            }
+            else if (!checkAudio.Checked)
+            {
+                soundPlayer.Stop();
+            }
+
+           
+
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            // Specify the path of the compiled main.exe file
+            string exePath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+
+            // Open the compiled .exe file with the default associated program
+            Process.Start(exePath);
+            this.Close();
         }
     }
 }
