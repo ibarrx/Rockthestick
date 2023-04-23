67f164e61ee690f037501b85e770c31d653f5cba
ibarrx <79670515+ibarrx@users.noreply.github.com>
2023-04-22 21:08:06 -0700

    Somewhat fixed

--- Menu/Properties/Resources.Designer.cs
+++ Menu/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Menu.Properties {
+namespace RockTheStick.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Menu.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Menu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RockTheStick.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
